7a46142b566b1ff4aa9e31ce17344d05d3298442
Mike Nelson <nelmedia@gmail.com>
2022-08-11 09:41:21 -0700

    tmr2022

---
+++ 2022-August_TechMentor Redmond/PowerShell Basics/Start-AlarmClock-Luke.ps1
@@ -0,0 +1,28 @@
+function Start-AlarmClock-Luke {
+    <#
+.SYNOPSIS
+An alarm with Star Wars messages!
+#>
+    param(
+        [int] $Seconds
+    )
+    Add-Type -AssemblyName System.Speech
+    $Synthesiser = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    "Alarm will trigger after $Seconds seconds, press <CTRL> C to stop the alarm"
+
+    Start-Sleep -Seconds $Seconds
+    while ($true) {
+      #         [console]::Beep()
+               $Synthesiser.Speak("Wake up Luke! May the force be with you! By the way, Darth Vader is your father.")
+                [console]::beep(440, 500)
+                [console]::beep(440, 500)
+                [console]::beep(440, 500)
+                [console]::beep(349, 350)
+                [console]::beep(523, 150)
+                [console]::beep(440, 500)
+                [console]::beep(349, 350)
+                [console]::beep(523, 150)
+                [console]::beep(440, 1000)
+    }
+}
+Start-AlarmClock-Luke
---
+++ 2022-August_TechMentor Redmond/PowerShell Basics/demo-TMR2022.ps1
@@ -0,0 +1,134 @@
+Set-Location c:\powershell
+
+Get-Process
+
+Show-Command Get-Process
+
+Get-Process | Get-Member
+
+# Modules
+Get-Module
+Get-Module -ListAvailable
+
+Import-Module WSL
+Get-Command -Module WSL
+
+# Help
+Get-Help Get-Process
+Get-Help Get-Process -Examples
+Get-Help Get-Process -Full
+
+# Pipeline
+notepad.exe
+Get-Process notepad | Stop-Process
+
+Get-ChildItem -Path *.txt | Where-Object {$_.length -gt 10000} | Sort-Object -Property length | Format-Table -Property name, length
+
+# Get-Help & start function
+Get-Help Start-Sleep
+Get-Help Start-Sleep -Parameter Seconds
+Get-Alias -Definition Start-Sleep
+sleep 3
+sleep 3; [console]::Beep()
+sleep 3; while (1) { [console]::Beep() }
+
+code SimpleAlarmClock.ps1
+
+# Show "One-line" function loaded in memory
+
+function Start-AlarmClock { Start-Sleep -Seconds 3 while ($true) { [console]::Beep() } }
+
+# PSDrive dir of Function:
+Get-PSDrive
+dir Function:
+dir Function:\*alarm*
+
+# Add seconds parameter
+function Start-AlarmClock {
+    param(
+        [int] $Seconds
+    )
+
+    Start-Sleep -Seconds $Seconds
+    while ($true) {
+        [console]::Beep()
+    }
+}
+
+Start-AlarmClock 3
+
+# Add Synopsis
+function Start-AlarmClock {
+    <#
+ .SYNOPSIS
+ After the parameter of number of seconds has elapsed, this function will beep until user initiates a <CTRL>C
+ #>
+    param(
+        [int] $Seconds
+    )
+
+    Start-Sleep -Seconds $Seconds
+    while ($true) {
+        [console]::Beep()
+    }
+}
+
+# See Synopsis added for help
+Get-Help Start-AlarmClock
+
+# Show the code - show the invoke
+code .\Start-AlarmClock.ps1
+
+# Run it - doesn't do anything???
+Start-AlarmClock.ps1
+# Need to dot-source the function - it makes the function available in the global scope
+.\Start-AlarmClockScript.ps1
+# add seconds
+.\Start-AlarmClock -Seconds 3
+
+# Luke
+.\Start-AlarmClock-Luke.ps1
+
+# Show the code if time
+code .\Start-AlarmClock-Luke.ps1
+
+# Let's start building our Module
+# Just creating a directory for the module
+New-Item -Path AlarmClockModule -ItemType Directory
+
+# Copying the ps1 file and renaming it to .psm1
+Copy-Item -Path .\Start-AlarmClock.ps1 -Destination .\AlarmClockModule\AlarmClock.psm1
+
+# Importing the newly copied .psm1 file
+Import-Module .\AlarmClockModule\AlarmClock.psm1 -Verbose
+
+# Defining an array of variables for the manifest
+$Splat = @{
+    Path = '.\AlarmClockModule\AlarmClock.psd1'
+    Author = 'Mike'
+    ModuleVersion= '1.1.0.0'
+    FunctionsToExport = 'Start-AlarmClock'
+    Description = 'Demo - AlarmClock module'
+    RootModule = '.\AlarmClockModule\AlarmClock.psm1'
+}
+
+# Creating the manifest
+New-ModuleManifest @Splat
+
+# Open the manifest
+code .\AlarmClockModule\AlarmClock.psd1
+
+# Import the manifest, which loads the module
+Import-Module .\AlarmClockModule\AlarmClock.psd1 -Verbose
+
+# Module created!! Yea!
+
+# End demo
+
+Start-Process powershell -ArgumentList '-noprofile -noexit -command .\PSInvaders.ps1'
+
+.\Highway2Powershell.ps1
+
+https://www.youtube.com/watch?v=MWWzrp3xkYY
+
+
